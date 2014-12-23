54c10cf3b20e4a45e5f5bca057e208d33802da01
Utkarsh Upadhyay <musically.ut@gmail.com>
2014-12-23 23:55:16 +0100

    Provide a new way of adding middlewares.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,16 @@ module.exports = function(grunt) {
                 options: {
                     keepalive: true,
                     // ^^ This will PREVENT all subsequent tasks from running!
-                    middleware: function (connect) {
+
+                    middleware: function (connect, options, middlewares) {
+
+                        // If you are using the connect.options to serve static
+                        // files yourself, then you can use this technique
+                        // instead of hunting for which middlewares to retain.
+                        //
+                        // middlewares.unshift(delayRequestSnippet);
+                        // return middlewares;
+
                         return [
                                delayRequestSnippet
                           // , rewriteRequestSnippet
@@ -34,9 +43,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-connect-delay');
 
     // "configureDelayRules" should be before the "connect"/"express" task
-    grunt.registerTask('server',
-      [ 'configureDelayRules'
-      , 'connect:dev'
-      ]
-    );
+    grunt.registerTask('server', [ 'configureDelayRules' , 'connect:dev' ]);
+    grunt.registerTask('default', 'server');
 };
